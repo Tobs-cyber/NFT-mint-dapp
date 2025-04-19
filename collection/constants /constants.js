fe37140b41e6af3a462bc46d1db42c0eeeeded8c
Tobs GWNFT <58276582+Tobs-cyber@users.noreply.github.com>
2025-04-19 04:21:10 +0100

    second commit

---
+++ collection/constants /constants.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x880d39159b00586465609e8af4387b79b19422b2";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "http://nft-mint-dapp-seven.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
